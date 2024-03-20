1ea122a142f803c9e4389766a9f24c9414c45156
jjangdol-DGIST <jjangdol6954@gmail.com>
2024-03-20 06:54:14 +0000

    main: 이전 parsing 부분 ok

--- ComputerStructure/Assignment_1/main.cpp
+++ ComputerStructure/Assignment_1/main.cpp
@@ -8,58 +8,92 @@
 #include <iostream>
 #include <sstream>
 #include <regex>
+#include <map>
 
 #define MAX_LINE_LENGTH 100
 
 using namespace std;
 
-// string readText(const string &filename) {
-//     ifstream file(filename);
-//     string text;
-//     string line;
-//     if (file.is_open()) {
-//         while (getline(file, line)) {
-//             text += line;
-//             text += '\n';
-//         }
-//         file.close();
-//     } else {
-//         cerr << "Unable to open file: " << filename << endl;
-//     }
-//     return text;
-// }
+bool contains_hex_prefix(const string& str) {
+    return str.find("0x") != string::npos;
+}
+
+string remove_hex_prefix(const string& str) {
+    if (str.substr(0, 2) == "0x") {
+        return str.substr(2);
+    } else {
+        return str;
+    }
+}
+
+int instructions() {
+    return 0;
+}
 
 int main() {
     string filename = "sample/sample.s";
-    // string fileText = readText(filename);
     string line;
     ifstream file(filename);
     vector<string> words;
     string word;
     while (getline(file, line)) {
-        // cout << line << endl;
         stringstream sstream(line);
             while (getline(sstream, word, ' ')) {
-                word = regex_replace(word, std::regex("[,]"), "");
+                word = regex_replace(word, regex("[,:]"), "");
                 if (!word.empty()) {
                     words.push_back(word);
-                    // cout << words.back() << " : " << &words.back() << endl;
                 }
             }
     }
 
-    for (int i = 0; i < int(words.size()); i++) {
-        cout << words[i] << " : " << &words[i] << endl;
-    }
+    // 텍스트 출력하기
+    // for (int i = 0; i < int(words.size()); i++) {
+    //     cout << words[i] << " : " << &words[i] << endl;
+    // }
 
-    int main_pointer;
+    // cout << "======================================" << endl;
+
+    map<string, string*> label_map;
+    vector<string> data_seg;
+    int instruction_index = 0;
     for (int i = 0; i < int(words.size()); i++) {
-        if (words[i] == "main:") {
-            main_pointer = i;
+        if (words[i] == ".data") {
+        } else if (words[i] == ".text") {
+            instruction_index = i + 1;
             break;
+        } else if (words[i] != ".word") {
+            i += 2;
+            if (contains_hex_prefix(words[i])) {
+                data_seg.push_back(remove_hex_prefix(words[i]));
+                string *memory_address = &data_seg.back();
+                label_map[words[i - 2]] = memory_address;
+            } else {
+                data_seg.push_back(words[i]);
+                string *memory_address = &data_seg.back();          
+                label_map[words[i - 2]] = memory_address;
+            }
+        } else {
+            i++;
+            if (contains_hex_prefix(words[i])) {
+                data_seg.push_back(remove_hex_prefix(words[i]));
+            } else {
+                data_seg.push_back(words[i]);
+            }
         }
     }
-    cout << main_pointer << endl;
+
+    // label_map 출력
+    // for (const auto& pair : label_map) {
+    //     cout << pair.first << " : " << pair.second << endl;
+    // }
+
+    // data_seg 출력
+    // for (int i = 0;i < data_seg.size();i++) {
+    //     cout << data_seg[i] << endl;
+    // }
+
+    // 다음에 시작할 부분 출력
+    // cout << instruction_index << endl;
 
     return 0;
 }
