72f8098951b4c06c66de07801ec801e154199d61
jjangdol-DGIST <jjangdol6954@gmail.com>
2024-03-23 18:23:54 +0000

    중간 저장

--- ComputerStructure/Assignment_1/main.cpp
+++ ComputerStructure/Assignment_1/main.cpp
@@ -9,6 +9,7 @@
 #include <sstream>
 #include <regex>
 #include <map>
+#include <set>
 
 #define MAX_LINE_LENGTH 100
 
@@ -26,12 +27,29 @@ string remove_hex_prefix(const string& str) {
     }
 }
 
+void add_to_map(map<string, string*>& label_map, const string& key, const string& value) {
+    cout << key << " : " << &key << endl;
+    cout << value << " : " << &value << endl;
+    string* memory_address = const_cast<string*>(&value);
+    label_map[key] = memory_address;
+}
+
+bool is_instruction(const string &word) {
+    static const set<string> instructions = {
+        "addiu", "addu", "and", "andi", "beq", "bne", "j",
+        "jal", "jr", "lui", "lw", "la", "nor", "or",
+        "ori", "sltiu", "sltu", "sll", "srl", "sw", "subu",
+        "lb", "sb"
+    };
+    return instructions.count(word) > 0;
+}
+
 int instructions() {
     return 0;
 }
 
 int main() {
-    string filename = "sample/sample.s";
+    string filename = "sample/sample2.s";
     string line;
     ifstream file(filename);
     vector<string> words;
@@ -39,7 +57,7 @@ int main() {
     while (getline(file, line)) {
         stringstream sstream(line);
             while (getline(sstream, word, ' ')) {
-                word = regex_replace(word, regex("[,:]"), "");
+                word = regex_replace(word, regex("[,]"), "");
                 if (!word.empty()) {
                     words.push_back(word);
                 }
@@ -48,52 +66,69 @@ int main() {
 
     // 텍스트 출력하기
     // for (int i = 0; i < int(words.size()); i++) {
-    //     cout << words[i] << " : " << &words[i] << endl;
+    //     // cout << words[i] << " : " << &words[i] << endl;
+    //     cout << words[i] << endl;
     // }
-
-    // cout << "======================================" << endl;
 
     map<string, string*> label_map;
     vector<string> data_seg;
-    int instruction_index = 0;
+    vector<string> text_seg;
+    bool in_text = false;
+
+    bool is_label = 0;
+    string label_name;
+    int label_index;
+
     for (int i = 0; i < int(words.size()); i++) {
         if (words[i] == ".data") {
+            continue;
         } else if (words[i] == ".text") {
-            instruction_index = i + 1;
-            break;
-        } else if (words[i] != ".word") {
-            i += 2;
-            if (contains_hex_prefix(words[i])) {
-                data_seg.push_back(remove_hex_prefix(words[i]));
-                string *memory_address = &data_seg.back();
-                label_map[words[i - 2]] = memory_address;
-            } else {
-                data_seg.push_back(words[i]);
-                string *memory_address = &data_seg.back();          
-                label_map[words[i - 2]] = memory_address;
+            in_text = true;
+            continue;
+        }
+
+        if (in_text) {
+            if (words[i].back() == ':') {
+                string label = words[i].substr(0, words[i].size() - 1);
+                add_to_map(label_map, label, to_string(text_seg.size()));
+                continue;
             }
+            text_seg.push_back(words[i]);
         } else {
-            i++;
-            if (contains_hex_prefix(words[i])) {
-                data_seg.push_back(remove_hex_prefix(words[i]));
+            if (words[i] != ".word") {
+                i += 2;
+                if (contains_hex_prefix(words[i])) {
+                    data_seg.push_back(remove_hex_prefix(words[i]));
+                    add_to_map(label_map, regex_replace(words[i - 2], regex("[:]"), ""), words[i]);
+                } else {
+                    data_seg.push_back(words[i]);       
+                    add_to_map(label_map, regex_replace(words[i - 2], regex("[:]"), ""), words[i]);
+                }
             } else {
-                data_seg.push_back(words[i]);
+                i++;
+                if (contains_hex_prefix(words[i])) {
+                    data_seg.push_back(remove_hex_prefix(words[i]));
+                } else {
+                    data_seg.push_back(words[i]);
+                }
             }
         }
     }
 
     // label_map 출력
     // for (const auto& pair : label_map) {
-    //     cout << pair.first << " : " << pair.second << endl;
+    //     // cout << pair.first << " : " << pair.second << endl;
+    //     cout << pair.first << " : " << pair.second << "\n" << *(pair.second) << endl;
     // }
-
+    cout << "======================================" << endl;
     // data_seg 출력
-    // for (int i = 0;i < data_seg.size();i++) {
-    //     cout << data_seg[i] << endl;
+    for (size_t i = 0;i < data_seg.size();i++) {
+        cout << data_seg[i] << " : " << &(data_seg[i])<< endl;
+    }
+    // cout << "======================================" << endl;
+    // for (size_t i = 0;i < text_seg.size();i++) {
+    //     cout << text_seg[i] << endl;
     // }
-
-    // 다음에 시작할 부분 출력
-    // cout << instruction_index << endl;
 
     return 0;
 }
